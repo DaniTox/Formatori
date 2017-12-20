952f7b16a213a0385217dd3edf815905ffd1474d
Dani Tox <DaniTox@users.noreply.github.com>
2017-12-20 09:39:02 +0100

    updated Loader

--- Formatori/Model/Loader.swift
+++ Formatori/Model/Loader.swift
@@ -9,22 +9,38 @@
 import Foundation
 
 class Loader {
+
+    weak var delegate : LoaderDelegate?
     
     func createVerifica(materia: String, argomento: String, classe: String, data: Date) {
       //TODO SOSTITUIRE OR CON IL SIMBOLO DELL'OR
-      if materia.isEmpty OR argomento.isEmpty OR classe.isEmpty OR data.isEmpty { return }
+      if materia.isEmpty || argomento.isEmpty || classe.isEmpty { return }
       
       let link = Links.createVerifica
-      guard let url = URL(strin: link) else { return }
+      guard let url = URL(string: link) else { return }
       
-      URLSession.shared.dataTask( { (data, response, error)
-        
-        guard let data = data else { return }
-         if let json = try? JSONDecoder().decode(Response.self) {
-          	 
-         }
-                                  
-      }).resume()
+        URLSession.shared.dataTask(with: url) { [weak self] (data, response, error) in
+            guard let data = data else { return }
+            
+            let json = try? JSONDecoder().decode(Response.self, from: data)
+            
+            if json?.code != String(200) {
+                if let msg = json?.message {
+                    self?.delegate?.didCreateVerificaWithReturnCode!(1, and: msg)
+                }
+            }
+            else {
+                self?.delegate?.didCreateVerificaWithReturnCode!(0, and: nil)
+            }
+            
+        }.resume()
     }
     
 }
+
+
+@objc protocol LoaderDelegate {
+    @objc optional func didCreateVerificaWithReturnCode(_ code : Int, and message: String?)
+}
+
+
